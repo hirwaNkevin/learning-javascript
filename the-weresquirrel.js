9600acdd8303b8129eb6233e5ac6c4ea87e00223
hirwaNkevin <hirwankevin@gmail.com>
2023-06-01 13:15:32 +0200

    add day1 and description objects > the-weresquirrel.js

---
+++ the-weresquirrel.js
@@ -0,0 +1,9 @@
+let day1 = {
+    squirrel: false,
+    eventsDone: ["work", "touched tree", "pizza", "running"]
+};
+
+let descriptions = {
+    work: "Went to work",
+    "touched tree": "Touched a tree"
+}
