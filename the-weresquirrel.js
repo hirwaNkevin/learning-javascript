02526fee5b18871f7849d242d5a81ee8b2432725
hirwaNkevin <hirwankevin@gmail.com>
2023-06-01 13:35:53 +0200

    add pizza property and delete unwanted property descriptions

--- the-weresquirrel.js
+++ the-weresquirrel.js
@@ -7,3 +7,11 @@ let descriptions = {
     work: "Went to work",
     "touched tree": "Touched a tree"
 };
+
+descriptions.pizza = "Ate pizza";
+console.log(descriptions.pizza);
+
+descriptions.unwantedProperty = "unwanted";
+console.log(descriptions.unwantedProperty);
+delete descriptions.unwantedProperty;
+console.log(descriptions.unwantedProperty);
