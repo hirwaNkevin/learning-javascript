e1b5f8accfe9581e93e26fd5d6c473ae27f9c820
hirwaNkevin <hirwankevin@gmail.com>
2023-06-01 13:52:10 +0200

    start journal environment

--- the-weresquirrel.js
+++ the-weresquirrel.js
@@ -1,18 +1,4 @@
-let journal = [
-    {
-        events: ["work", "touched tree", "pizza",
-            "running", "television"],
-        squirrel: false
-    },
-    {
-        events: ["work", "ice cream", "cauliflower",
-            "lasagna", "touched tree", "brushed teeth"],
-        squirrel: false
-    },
-    {
-        events: ["weekend", "cycling", "break", "peanuts",
-            "beer"],
-        squirrel: true
-    },
-    /* and so on... */
-];
+let journal = [];
+function addEntry(events, squirrel) {
+journal.push({events, squirrel});
+}
