cd7dbae9a7f60f270d13f4571f51d3adf2532045
hirwaNkevin <hirwankevin@gmail.com>
2023-06-02 13:32:12 +0200

    Add code

---
+++ pyramid.js
@@ -0,0 +1,23 @@
+let printPyramid = function (size, character="^"){
+    let out = "";
+for (let i=0;i<size;i++){
+    let padCount = 0;
+    let padding = "";
+    while(padCount<(size-i)){
+        
+        padding += " ";
+        padCount++;
+    }
+    
+    if(i==0){
+        out+=`${character}`;
+        console.log(padding+out);
+    }
+    else{
+        out+=` ${character}`;
+        console.log(padding+out);
+    }
+}
+}
+
+printPyramid(3,"#")
