4572ae6050d5a510548dbcd3714a7026dfbdf6ba
hirwaNkevin <hirwankevin@gmail.com>
2023-06-09 11:27:53 +0200

    Add repeat with scramble function

--- practice/chapter1-4/replace-string.js
+++ practice/chapter1-4/replace-string.js
@@ -1,4 +1,20 @@
-let replaceString = function(word="word", letter="o", character="*"){ 
-    return(word.split(letter).join(character));
+let replaceString = function (word = "word", letter = "o", character = "*") {
+    return (word.split(letter).join(character));
 
+}
+
+function repeat(nberOfTimes, what) {
+    let repeated = [];
+    for (let i = 0; i < nberOfTimes; i++) {
+        repeated.push(what());
+    }
+    return repeated;
+}
+
+function scramble(name) {
+    let replaceString = function (word = "word", letter = "o", character = "*") {
+        return (word.split(letter).join(character));
+
+    }
+    return replaceString(name, 'a', 'u');
 }
