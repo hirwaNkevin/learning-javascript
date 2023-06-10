9e153f3ce3550ecf124f20a9dee7ec7380f4c46d
hirwaNkevin <hirwankevin@gmail.com>
2023-06-10 10:15:23 +0200

    Add comment

--- practice/higher-order-functions/chapter-exercises.js
+++ practice/higher-order-functions/chapter-exercises.js
@@ -2,8 +2,7 @@ function flatten(array){
     return array.reduce(
         (flatArray,innerArray)=>{
             for (let item of innerArray) {
-                flatArray.push(item);
-                
+                flatArray.push(item);// concat is not working
             }
             return flatArray
         },[]
