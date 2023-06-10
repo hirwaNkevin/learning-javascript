abcb8cace3ff7215f1d74feaa03b00f32673ca86
hirwaNkevin <hirwankevin@gmail.com>
2023-06-10 12:06:52 +0200

    Add loop function

--- practice/higher-order-functions/chapter-exercises.js
+++ practice/higher-order-functions/chapter-exercises.js
@@ -1,10 +1,27 @@
-function flatten(array){
+function flatten(array) {
     return array.reduce(
-        (flatArray,innerArray)=>{
+        (flatArray, innerArray) => {
             for (let item of innerArray) {
                 flatArray.push(item);// concat is not working
             }
             return flatArray
-        },[]
+        }, []
     );
 }
+
+// a higher order function smthng like a for loop
+function loop(value, test, update, body) {
+    let allowed = false;
+    do {
+        if (test(value)) {
+            allowed = true;
+            console.log(allowed);
+            body(value);
+        }
+        else { return undefined }
+
+        value = update(value);
+        console.log(value);
+    } while (allowed)
+}
+loop(7, (value) => value > 0, (value) => { value--; }, (value) => { console.log(value) });
