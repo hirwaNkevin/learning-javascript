a227ef9ec52385ca5546e68290714267853b3736
hirwaNkevin <hirwankevin@gmail.com>
2023-06-13 20:55:08 +0200

    Solve dominant direction question

--- practice/higher-order-functions/chapter-exercises.js
+++ practice/higher-order-functions/chapter-exercises.js
@@ -35,3 +35,17 @@ function every_LoopVersion(array,predicate){
     }
     return result;
 }
+
+function dominantWritingDirection(text) {
+    let scripts = countBy(text, (char) => {
+        let script = characterScript(char.codePointAt(0));
+        return script ? script.direction : 'none';
+    }).filter(({ direction }) => direction != "none");
+
+    let total = scripts.reduce((n, { count }) => n + count, 0);
+    if (total == 0) return 'no scripts found';
+
+    return scripts.reduce((a, b) => a.count > b.count ? a : b);
+
+}
+// console.log(dominantWritingDirection('英国的狗说"woof", 俄罗斯的狗说"тяв"'));
