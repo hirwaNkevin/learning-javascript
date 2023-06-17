641000b83fe12562cebf986af1a35c89785e4dee
hirwaNkevin <hirwankevin@gmail.com>
2023-06-17 13:15:17 +0200

    Add walk function

---
+++ practice/secret-life-of-objects/practice.js
@@ -0,0 +1,14 @@
+function walk(distance,direction) {
+    if(direction==="y"){
+        this.position[1]<=0 ? this.position : this.position[1]+=distance;
+        return this.position;
+    }
+    else if(direction==="x"){
+        this.position[0]+=distance;
+        return this.position
+    }
+    else {
+        this.position[1]+=distance;
+        return this.position;
+    }
+}
