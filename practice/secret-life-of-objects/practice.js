843ea0caab2d71063d6483b5a81028f60d9496a4
hirwaNkevin <hirwankevin@gmail.com>
2023-06-19 11:32:17 +0200

    Format document

--- practice/secret-life-of-objects/practice.js
+++ practice/secret-life-of-objects/practice.js
@@ -1,23 +1,23 @@
 // methods
-let cow = {position: [0,0],walk};
+let cow = { position: [0, 0], walk };
 
-function walk(distance,direction) {
-    if(direction==="y"){
-        this.position[1]<=0 ? this.position : this.position[1]+=distance;
+function walk(distance, direction) {
+    if (direction === "y") {
+        this.position[1] <= 0 ? this.position : this.position[1] += distance;
         return this.position;
     }
-    else if(direction==="x"){
-        this.position[0]+=distance;
+    else if (direction === "x") {
+        this.position[0] += distance;
         return this.position
     }
     else {
-        this.position[1]+=distance;
+        this.position[1] += distance;
         return this.position;
     }
 }
 
 // prototypes
-let animal = {kind:'animal', position:[0,0], sound(sound){return sound}, walk};
+let animal = { kind: 'animal', position: [0, 0], sound(sound) { return sound }, walk };
 let dog = Object.create(animal);
 dog.kind = 'Dog';
 dog.walk(1);
